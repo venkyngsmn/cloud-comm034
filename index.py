42a72dabeecf148437e5a2c1618752cd1286c20b
venkyngsmn <61684277+venkyngsmn@users.noreply.github.com>
2023-08-09 19:32:22 +0100

    Add files via upload

--- index.py
+++ index.py
@@ -66,48 +66,30 @@ data = data.reset_index()
 data['Date'] = data['Date'].dt.strftime('%Y-%m-%d')
 dict_data = data.to_dict(orient='list')
 
-global ins_ids
-ins_ids = []	
-
+# Rendering template function
 def doRender(tname, values={}):
 	if not os.path.isfile(os.path.join(os.getcwd(), 'templates/'+tname)):
 		return render_template('index.htm')
 	return render_template(tname, **values)
 
 
+# Function of the 'warmup'
 @app.route('/calculate', methods=['POST'])
-def calculateHandler():
+def initHandler():
 	if request.method == 'POST':
-		ls = ['s', 'r', 'h', 'd', 't', 'p']
-		s, r, h, d, t, p  = [request.form.get(i) for i in ls]
 		
-		json_inputs = '{"key1":"'+str(dict_data['Date'])+'","key2":"'+str(dict_data['Close'])+'","key3":"'+str(dict_data['Buy'])+'","key4":"'+str(dict_data['Sell'])+'","key5":"'+str(h)+'","key6":"'+str(d)+'","key7":"'+str(t)+'","key8":"'+str(r)+'"}'
+		global s
+		global r
+		s = request.form.get('s')
+		r = request.form.get('r')
 		
-		
-		
+		# warming up lambda
 		if s == 'lambda':
-			def getpage(id):
-				conn = http.client.HTTPSConnection("hs02ojgqx9.execute-api.us-east-1.amazonaws.com")
-				# json_inputs = '{"key1":"'+str(dict_data['Date'])+'","key2":"'+str(dict_data['Close'])+'","key3":"'+str(dict_data['Buy'])+'","key4":"'+str(dict_data['Sell'])+'","key5":"'+str(h)+'","key6":"'+str(d)+'","key7":"'+str(t)+'","key8":"'+str(r)+'"}'
-				conn.request("POST", "/default/simulate", json_inputs)   
-				response = conn.getresponse()
-				data = response.read().decode('utf-8')
-				result = json.loads(data)
-				return result
-			runs = [i for i in range(int(r))]
-			def getpages():
-				with ThreadPoolExecutor() as executor:
-					results = executor.map(getpage, runs)
-				return results
+			conn = http.client.HTTPSConnection("hs02ojgqx9.execute-api.us-east-1.amazonaws.com")
+			return doRender('form.htm', {'note': "Connected to "+ str(conn)})
 			
-			start = time.time()
-			results = getpages()
-			time_taken = time.time() - start
-			
-			cost = "$ "+str(float(r) * 128 / 1024 * time_taken * 0.0000166667)
-		
-			
-		elif s=='ec2':
+		# warming up EC2 instances
+		elif s == 'ec2':
 			ec2 = boto3.resource('ec2', region_name='us-east-1')
 			user_data = """#!/bin/bash
 					apt update -y
@@ -121,7 +103,8 @@ def calculateHandler():
 
 					a2enmod cgi
 					service apache2 restart"""
-			start = time.time()
+			global st
+			st = time.time()
 			instances = ec2.create_instances(        
 				ImageId = 'ami-001a7d2a632ee2e35',
 				MinCount = int(r),
@@ -131,18 +114,52 @@ def calculateHandler():
 				SecurityGroups = ['SSH'],
 				UserData = user_data
 				)
+			global dnss
 			dnss = []
 			for i in instances:
 				i.wait_until_running()
 				i.load()
 				dnss.append(i.public_dns_name)
 			time.sleep(60)
+			return doRender('form.htm', {'note': "Currently running "+ str(len(dnss)) + " EC2 instances"})
+		
+@app.route('/results', methods=['POST'])
+def calculateHandler():
+	if request.method == 'POST':
+		ls = ['h', 'd', 't', 'p']
+		h, d, t, p  = [request.form.get(i) for i in ls]
+		
+		json_inputs = '{"key1":"'+str(dict_data['Date'])+'","key2":"'+str(dict_data['Close'])+'","key3":"'+str(dict_data['Buy'])+'","key4":"'+str(dict_data['Sell'])+'","key5":"'+str(h)+'","key6":"'+str(d)+'","key7":"'+str(t)+'"}'
+		
+		# AWS Lambda execution
+		if s == 'lambda':
+			def getpage(id):
+				conn = http.client.HTTPSConnection("hs02ojgqx9.execute-api.us-east-1.amazonaws.com")
+				conn.request("POST", "/default/simulate", json_inputs)   
+				response = conn.getresponse()
+				data = response.read().decode('utf-8')
+				result = json.loads(data)
+				return result
+			runs = list(range(int(r)))
+			def getpages():
+				with ThreadPoolExecutor() as executor:
+					results = executor.map(getpage, runs)
+				return results
+			
+			start = time.time()
+			results = list(getpages())
+			
+			time_taken = time.time() - start
+			
+			cost = "$ "+str(float(r) * 128 / 1024 * time_taken * 0.0000166667)
+		
+		# AWS EC2 execution
+		elif s=='ec2':
+
 			results = []     
-			for url in dnss:
+			def getpage(url):
 				ec2_url = "http://" + url + "/aws_ec2.py"
-				print(ec2_url)
 				json_output = requests.post(ec2_url, headers={"Content-Type":"application/json"}, data=json_inputs)
-				
 				output = json.loads(json_output.text)
 				
 				result= [] 
@@ -150,13 +167,18 @@ def calculateHandler():
 				result.append(output['var95'])
 				result.append(output['var99'])
 				
-				results.append(result)
-				
-			time_taken = time.time() - start
+				return result
+			def getpages():
+				with ThreadPoolExecutor() as executor:
+					results = executor.map(getpage, dnss)
+				return results
+			results = getpages()
+			time_taken = time.time() - st
 			
 			cost = "$" + str(float(r) * 0.0134 * time_taken / 60)
 			
 		
+		# Averaging the results
 		pl = []
 		dates = []
 		var95s = []
@@ -177,7 +199,7 @@ def calculateHandler():
 				
 			if t =='sell': 
 				profit = -1*profit
-			pl.append(round(profit,2))
+			pl.append(float(profit))
 		
 		var95_avgd = []
 		var99_avgd = []
@@ -192,6 +214,8 @@ def calculateHandler():
 		
 		note = list(zip(dates, var95_avgd, var99_avgd, pl))
 		
+		
+		# Creating the charts using the image-charts api
 		str_d = '|'.join(dates)
 		str_95 = ','.join([str(i) for i in var95_avgd])
 		str_avg95 = ','.join([str(avg95) for i in range(len(dates))])
@@ -202,11 +226,14 @@ def calculateHandler():
 		chart = f"https://image-charts.com/chart?cht=lc&chs=999x499&chd=a:{str_95}|{str_99}|{str_avg95}|{str_avg99}&chxt=x,y&chdl={labels}&chxl=0:|{str_d}&chxs=0,min90&chco=1984C5,C23728,A7D5ED,E1A692&chls=3|3|3,5,3|3,5,3"
 
 		
+		# audit page bucket
 		s3 = boto3.client('s3') 
 		# retrieving the previous data from the bucket
 		previous = s3.get_object(Bucket="hashbang14", Key="audit_data.json")
 		pre_data = previous['Body'].read()
 		pre = json.loads(pre_data)
+		pft = sum([float(i) for i in pl if not isinstance(i, str)])
+		print(pft)
 		
 		pre['s'].append(s)
 		pre['r'].append(r)
@@ -216,7 +243,7 @@ def calculateHandler():
 		pre['p'].append(int(p))
 		pre['avg95'].append(avg95) 
 		pre['avg99'].append(avg99)
-		pre['profit'].append([i for i in pl if isinstance(i, str)])
+		pre['profit'].append(pft)
 		pre['cost'].append(cost)
 		pre['time'].append(time_taken)
 		
@@ -226,7 +253,7 @@ def calculateHandler():
 		s3.put_object(Body=str_json.encode('utf-8'), Bucket="hashbang14", Key="audit_data.json")
 		
 		
-		return doRender('results.htm', {'note': note, 'chart':chart, 's':s, 'r':r, 'h':h, 'd':d, 't':t, 'p':p, 'avg95':avg95, 'avg99':avg99, 'cost':cost, 'time': time_taken})
+		return doRender('results.htm', {'note': note, 'chart':chart, 's':s, 'r':r, 'h':h, 'd':d, 't':t, 'p':p, 'avg95':avg95, 'avg99':avg99, 'cost':cost, 'time': time_taken, 'profit':pft})
 		
 @app.route('/audit')
 def auditHandler():	
@@ -234,14 +261,20 @@ def auditHandler():
 	data = s3.get_object(Bucket='hashbang14', Key='audit_data.json')
 	result = json.loads(data['Body'].read())
 	# result = json.loads(result)
-	vals = list(zip(result['s'], result['r'], result['h'], result['d'], result['t'], result['p'], result['avg95'], result['avg99'], result['cost'], result['time'])) 
+	vals = list(zip(result['s'], result['r'], result['h'], result['d'], result['t'], result['p'], result['avg95'], result['avg99'], result['cost'], result['time'], result['profit'])) 
 	return doRender('audit.htm', {'note':vals})
 		
 @app.route('/terminate')
 def terminateHandler():
-	ec2 = boto3.client('ec2')
-	response = ec2.terminate_instances(InstanceIds = ins_ids)
-	return doRender("index.htm", {"note": "Terminated all instances"})
+	ec2 = boto3.resource('ec2', region_name="us-east-1")
+	filters = [{'Name': 'instance-state-name', 'Values': ['running']}]
+	instances = ec2.instances.filter(Filters=filters)
+	n = len(list(instances))
+	if n:
+		response = ec2.instances.filter(InstanceIds = [i.id for i in instances]).terminate()
+		return doRender("index.htm", {"note": "Terminated " + str(n) + " instances"})
+	else:
+		return doRender("index.htm", {"note": "No instances are currently running"})
 		
 '''
 @app.route('/cacheavoid/<name>')
--- static/aws_ec2.py
+++ static/aws_ec2.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import math, random, sys, json
 from statistics import mean, stdev
 
@@ -5,59 +7,59 @@ from statistics import mean, stdev
 event = json.loads(sys.stdin.read())
 
 
-dt = event['key1']
-close = event['key2']
-buy = event['key3']
-sell = event['key4']
-h = event['key5']
-d = event['key6']
+dt = eval(event['key1'])
+close = eval(event['key2'])
+buy = eval(event['key3'])
+sell = eval(event['key4'])
+h = int(event['key5'])
+d = int(event['key6'])
 t = event['key7']
-r = event['key8']
 minhistory = h
 shots = d
 var95_list = []
 var99_list = []
 dates = []
+
 for i in range(minhistory, len(close)):
 	if t == "buy":
-	    if buy[i] == 1: # if we’re interested in Buy signals
-		close_data = close[i-minhistory:i]
-		pct_change = [(close_data[i] - close_data[i-1]) / close_data[i-1] for i in range(1,len(close_data))]
-		mn = mean(pct_change)
-		std = stdev(pct_change)
-		# generate much larger random number series with same broad characteristics 
-		simulated = [random.gauss(mn,std) for x in range(shots)]
-		# sort and pick 95% and 99%  - not distinguishing long/short risks here
-		simulated.sort(reverse=True)
-		var95 = simulated[int(len(simulated)*0.95)]
-		var99 = simulated[int(len(simulated)*0.99)]
-		var95_list.append(var95)
-		var99_list.append(var99)
-		dates.append(str(dt[i]))
+		if buy[i] == 1: # if we’re interested in Buy signals
+			close_data = close[i-minhistory:i]
+			pct_change = [(close_data[i] - close_data[i-1]) / close_data[i-1] for i in range(1,len(close_data))]
+			mn = mean(pct_change)
+			std = stdev(pct_change)
+			# generate much larger random number series with same broad characteristics 
+			simulated = [random.gauss(mn,std) for x in range(shots)]
+			# sort and pick 95% and 99%  - not distinguishing long/short risks here
+			simulated.sort(reverse=True)
+			var95 = simulated[int(len(simulated)*0.95)]
+			var99 = simulated[int(len(simulated)*0.99)]
+			var95_list.append(var95)
+			var99_list.append(var99)
+			dates.append(str(dt[i]))
 	elif t == "sell":
-	    if sell[i] == 1: # if we’re interested in Sell signals
-		close_data = close[i-minhistory:i]
-		pct_change = [(close_data[i] - close_data[i-1]) / close_data[i-1] for i in range(1,len(close_data))]
-		mn = mean(pct_change)
-		std = stdev(pct_change)
-		# generate much larger random number series with same broad characteristics 
-		simulated = [random.gauss(mn,std) for x in range(shots)]
-		# sort and pick 95% and 99%  - not distinguishing long/short risks here
-		simulated.sort(reverse=True)
-		var95 = simulated[int(len(simulated)*0.95)]
-		var99 = simulated[int(len(simulated)*0.99)]
-		var95_list.append(var95)
-		var99_list.append(var99)
-		dates.append(str(dt[i]))
+		if sell[i] == 1: # if we’re interested in Sell signals
+			close_data = close[i-minhistory:i]
+			pct_change = [(close_data[i] - close_data[i-1]) / close_data[i-1] for i in range(1,len(close_data))]
+			mn = mean(pct_change)
+			std = stdev(pct_change)
+			# generate much larger random number series with same broad characteristics 
+			simulated = [random.gauss(mn,std) for x in range(shots)]
+			# sort and pick 95% and 99%  - not distinguishing long/short risks here
+			simulated.sort(reverse=True)
+			var95 = simulated[int(len(simulated)*0.95)]
+			var99 = simulated[int(len(simulated)*0.99)]
+			var95_list.append(var95)
+			var99_list.append(var99)
+			dates.append(str(dt[i]))
 
 output = {"dates" : dates,
-	"var95" : var95,
-	"var99" : var99
-	}
+		"var95" : var95_list,
+		"var99" : var99_list
+		}
 
 output = json.dumps(output)
 
 print("Content-Type: application/json")
-print("")
+print()
 print(output)
 
